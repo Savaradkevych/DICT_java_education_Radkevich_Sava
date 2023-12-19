1607992a1603f0c97b8bf81a267955918f533748
Savaradkevych <s.v.radkevych@student.khai.edu>
2023-12-19 07:49:10 +0200

    Hangman 3-rd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
@@ -7,12 +8,14 @@ public class Hangman {
         System.out.println("\nWelcome to Hangman! Can you guess the word and survive?");
         System.out.println("Stay tuned for the game release!");
 
-        // Етап 2: Спрощена версія гри
-        playSimplifiedHangman();
+        // Етап 3: Ускладнена версія гри - вибір випадкового слова зі списку
+        playAdvancedHangman();
     }
 
-    public static void playSimplifiedHangman() {
-        String secretWord = "java"; // Задане слово для вгадування
+    public static void playAdvancedHangman() {
+        String[] words = {"python", "java", "javascript", "kotlin"};
+        String secretWord = getRandomWord(words);
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("\nGuess the word: > ");
@@ -21,7 +24,13 @@ public class Hangman {
         if (userGuess.equals(secretWord)) {
             System.out.println("You survived!");
         } else {
-            System.out.println("You lost!");
+            System.out.println("You lost! The correct word was: " + secretWord);
         }
+    }
+
+    private static String getRandomWord(String[] words) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.length);
+        return words[randomIndex];
     }
 }
