b7fa03738a3c7b0280109b879c0be246bb4c0c07
Savaradkevych <s.v.radkevych@student.khai.edu>
2023-12-19 08:27:01 +0200

    CoffeeMachine 2-nd stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
         makeCoffee();
+        calculateIngredients();
     }
 
     public static void makeCoffee() {
@@ -11,5 +14,23 @@ public class CoffeeMachine {
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");
+    }
+
+    public static void calculateIngredients() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nWrite how many cups of coffee you will need:");
+        int cups = scanner.nextInt();
+
+        int water = cups * 200;
+        int milk = cups * 50;
+        int coffeeBeans = cups * 15;
+
+        System.out.println("For " + cups + " cups of coffee you will need:");
+        System.out.println(water + " ml of water");
+        System.out.println(milk + " ml of milk");
+        System.out.println(coffeeBeans + " g of coffee beans");
+
+        scanner.close();
     }
 }
