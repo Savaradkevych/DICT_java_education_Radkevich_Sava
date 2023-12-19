ee022b8282cc552b662cc0c5534f22765116dbe5
Savaradkevych <s.v.radkevych@student.khai.edu>
2023-12-19 07:50:08 +0200

    Hangman 4-th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -8,17 +8,18 @@ public class Hangman {
         System.out.println("\nWelcome to Hangman! Can you guess the word and survive?");
         System.out.println("Stay tuned for the game release!");
 
-        // Етап 3: Ускладнена версія гри - вибір випадкового слова зі списку
-        playAdvancedHangman();
+        // Етап 4: Додана підказка
+        playHintHangman();
     }
 
-    public static void playAdvancedHangman() {
+    public static void playHintHangman() {
         String[] words = {"python", "java", "javascript", "kotlin"};
         String secretWord = getRandomWord(words);
+        String guessedWord = getHint(secretWord);
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("\nGuess the word: > ");
+        System.out.print("\nGuess the word " + guessedWord + ": > ");
         String userGuess = scanner.nextLine().toLowerCase();
 
         if (userGuess.equals(secretWord)) {
@@ -26,6 +27,18 @@ public class Hangman {
         } else {
             System.out.println("You lost! The correct word was: " + secretWord);
         }
+    }
+
+    private static String getHint(String word) {
+        StringBuilder hint = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if (i < 2) {
+                hint.append(word.charAt(i));
+            } else {
+                hint.append("-");
+            }
+        }
+        return hint.toString();
     }
 
     private static String getRandomWord(String[] words) {
