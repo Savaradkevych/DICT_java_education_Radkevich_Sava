990e13a98ba161a537c6aaeeed0fa8ca45de9700
Savaradkevych <s.v.radkevych@student.khai.edu>
2023-12-19 07:39:17 +0200

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,10 +1,8 @@
 import java.time.Year;
+import java.util.Scanner;
 
 public class ChatBot {
-    // Ім'я бота
     private String botName;
-
-    // Рік створення бота
     private int birthYear;
 
     // Конструктор класу
@@ -17,6 +15,21 @@ public class ChatBot {
     public void printGreeting() {
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+        askForName();
+    }
+
+    // Метод для запитання імені користувача
+    private void askForName() {
+        System.out.println("Please, remind me your name.");
+
+        // Створення об'єкту Scanner для зчитування введення користувача
+        Scanner scanner = new Scanner(System.in);
+
+        // Зчитування імені користувача зі стандартного введення
+        String userName = scanner.nextLine();
+
+        // Виведення вітання з ім'ям користувача
+        System.out.println("What a great name you have, " + userName + "!");
     }
 
     public static void main(String[] args) {
